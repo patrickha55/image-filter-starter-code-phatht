365ad799b7c5f1aba17c4cb78632c76a110e4afa
PatrickHa <pattrickha@gmail.com>
2022-10-08 10:54:45 +0700

    Added image filtered feature

---
+++ src/controller/V0/image/routes/image.router.ts
@@ -0,0 +1,27 @@
+import { Router, Request, Response } from 'express';
+import { deleteLocalFiles, filterImageFromURL } from '../../../../util/util';
+
+const router: Router = Router();
+
+//Filter an Image
+router.get('/filteredimage', async (req : Request, res : Response) => {
+    const { image_url } = req.query;
+
+    if (!image_url) {
+        return res.status(400).send('Invalid image url.');
+    }
+
+    var filteredImage = await filterImageFromURL(image_url);
+
+    if (!filterImageFromURL) {
+        return res.status(500).send('Something went wrong');
+    }
+
+    res.status(200).sendFile(filteredImage);
+
+    return res.on('finish', () => {
+        deleteLocalFiles([filteredImage]);
+    });
+});
+
+export const ImageRouter: Router = router;
